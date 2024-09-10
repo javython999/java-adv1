9ab858c31dbbd14c9c3297732119010be4606f4e
javython999 <infinity2544@gmail.com>
2024-09-10 20:57:49 +0900

    LockSupport, Lock Interface, ReentrantLock

--- src/thread/sync/BankMain.java
+++ src/thread/sync/BankMain.java
@@ -6,7 +6,12 @@ import static util.ThreadUtils.sleep;
 public class BankMain {
 
     public static void main(String[] args) throws InterruptedException {
-        BankAccount account = new BankAcountV3(1000);
+        //BankAccount account = new BankAcountV1(1000);
+        //BankAccount account = new BankAcountV2(1000);
+        //BankAccount account = new BankAcountV3(1000);
+        //BankAccount account = new BankAcountV4(1000);
+        //BankAccount account = new BankAcountV5(1000);
+        BankAccount account = new BankAcountV6(1000);
 
         Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
         Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
